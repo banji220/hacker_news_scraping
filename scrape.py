3b6fe61e3906bc3ce16322ca519d7b278de18397
Banji <khanjani1997@gmail.com>
2020-08-18 14:14:55 +0430

    Making a new function 'sort_by_votes' to sort articles from highest score to the lowest

--- scrape.py
+++ scrape.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+import pprint
 response = requests.get("https://news.ycombinator.com/")
 soup = BeautifulSoup(response.text, "html.parser")
 
@@ -8,15 +8,20 @@ soup = BeautifulSoup(response.text, "html.parser")
 links = soup.select(".storylink ")
 votes = soup.select(".score")
 
+# Making a function to sort votes from highes to lowest
+def sort_by_votes(hnlist):
+    return sorted(hnlist, key = lambda k:k["votes"])
+
 # Making a function with two parameted
 def hackernews(links, votes):
     hn = []
     for idx, item in enumerate(links):
+        # more_link = soup.select(".morelink")
         title = links[idx].getText()
         href = links[idx].get("href", None)
         points = int(votes[idx].getText().replace(" points", ""))
         if points > 100:
             hn.append({"title": title, "link": href, "votes": points})
-    return hn
+    return sort_by_votes(hn)
 
-print(hackernews(links, votes))
+pprint.pprint(hackernews(links, votes))

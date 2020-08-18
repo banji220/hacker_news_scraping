2791a34c1bd6fd10a2e7e98b2332685c5845ad99
Banji <khanjani1997@gmail.com>
2020-08-18 12:58:47 +0430

    Adding 'if' statement to get articles with 100 votes and more

--- scrape.py
+++ scrape.py
@@ -13,7 +13,10 @@ def hackernews(links, votes):
     hn = []
     for idx, item in enumerate(links):
         title = links[idx].getText()
-        hn.append(title)
+        href = links[idx].get("href", None)
+        points = int(votes[idx].getText().replace(" points", ""))
+        if points > 100:
+            hn.append({"title": title, "link": href, "votes": points})
     return hn
 
 print(hackernews(links, votes))
